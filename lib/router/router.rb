41b8b05a3d60a35e29ea7b6d4f9d1abdf521af55
Jason Thompson <jason@jthompson.ca>
2013-10-04 05:33:16 -0400

    Finish spike

--- lib/router/router.rb
+++ lib/router/router.rb
@@ -9,20 +9,34 @@ module Router
     def call(env)
       req = Rack::Request.new(env)
 
-      msg = route(req.path_info)
+      msg = matcher(req.path_info)
       res = Rack::Response.new
       res.write(msg)
       res.finish
     end
 
-    def route(path_info)
-      routes[path_info]
+    def matcher(path)
+      Route.new(path, routes).match
     end
 
     def routes
       {
-        '/2013/02/this-is-a-title' => 'blog post!'
+        '/2013/02/this-is-a-title' => 'blog post!',
+        '/:year/:month/:title' => "blog post in march"
         }
+    end
+
+    class Route
+      attr_reader :path, :routes
+      
+      def initialize(path, routes)
+        @path = path
+        @routes = routes
+      end
+
+      def match
+        routes[path]
+      end
     end
   end
 end
