814d611b0151e4081627a0454dccaad778856553
Jason Thompson <jason@jthompson.ca>
2013-10-04 06:37:42 -0400

    Add

--- lib/router/router.rb
+++ lib/router/router.rb
@@ -1,42 +1,32 @@
 module Router
   class Router
-    attr_reader :app
-
     def initialize(app)
-      @app = app
+       @app = app
     end
 
-    def call(env)
-      req = Rack::Request.new(env)
+    # def call(env)
+    #   req = Rack::Request.new(env)
 
-      msg = matcher(req.path_info)
-      res = Rack::Response.new
-      res.write(msg)
-      res.finish
+    #   res = Rack::Response.new
+    #   res.write(path_info)
+    #   res.finish
+    # end
+
+    def route(path, &block)
+      path_params = get_path_params path
+      resource = path.split('/')[0]
+      puts path_params.to_s + resource
     end
-
-    def matcher(path)
-      Route.new(path, routes).match
+    # Todo: get resource (first item always empty string).
+    # Is next item a path_param? If so, resource is '/'.
+    
+    private
+    
+    def get_path_params(path)
+      regex = /(?<=:)\w+/
+      path_params = path.scan regex
+      path_params.collect {|p| p.intern}
     end
-
-    def routes
-      {
-        '/2013/02/this-is-a-title' => 'blog post!',
-        '/:year/:month/:title' => "blog post in march"
-        }
-    end
-
-    class Route
-      attr_reader :path, :routes
       
-      def initialize(path, routes)
-        @path = path
-        @routes = routes
-      end
-
-      def match
-        routes[path]
-      end
-    end
   end
 end
